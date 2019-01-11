c7fa0941924c5efb943414fa1610fd290aa9dbeb
chenjiandongx <chenjiandongx@qq.com>
2019-01-11 11:08:03 +0800

    Fix: 变量重复命名

--- priority_queue_test.go
+++ priority_queue_test.go
@@ -10,25 +10,26 @@ import (
 func TestPriorityQueue(t *testing.T) {
 	q := NewPriorityQueue()
 
+	var item1, item2 interface{}
+	var ok bool
+
 	for i := 0; i < nums; i++ {
 		r := rand.Int()
 		q.Put(&PqNode{Value: string(r), Priority: rand.Int()})
 	}
 
 	for i := 0; i < nums/2; i++ {
-		item1, ok := q.Get()
-		item2, ok := q.Get()
+		item1, ok = q.Get()
+		item2, ok = q.Get()
 		assert.True(t, item1.(*PqNode).Priority > item2.(*PqNode).Priority)
 		assert.Equal(t, ok, true)
 	}
-	var item interface{}
-	var ok bool
 
-	item, ok = q.Get()
-	assert.Equal(t, nil, item)
+	item1, ok = q.Get()
+	assert.Equal(t, nil, item1)
 	assert.Equal(t, ok, false)
 
-	item, ok = q.Get()
-	assert.Equal(t, nil, item)
+	item1, ok = q.Get()
+	assert.Equal(t, nil, item1)
 	assert.Equal(t, ok, false)
 }
